5560c8915b8a115dcb65c6a923ce141819feccc8
Exaucé <47887636+exauce21@users.noreply.github.com>
2026-09-09 11:07:51 +0200

    update projet

--- dist/middlewares/logger.js
+++
@@ -1,5 +0,0 @@
-export const logger = (req, res, next) => {
-    console.log(`${new Date().toISOString()} ${req.method} ${req.originalUrl}`);
-    next();
-};
-//# sourceMappingURL=logger.js.map
--- dist/middlewares/visit-counter.js
+++
@@ -1,7 +0,0 @@
-let visits = 0;
-export const visitCounter = (req, res, next) => {
-    req.visit = ++visits;
-    res.setHeader("X-Visit", req.visit);
-    next();
-};
-//# sourceMappingURL=visit-counter.js.map
